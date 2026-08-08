b40222dd9ea6aaff12b4628f77baeb5de956cafc
Maksym <maksim260306@gmail.com>
2026-08-08 16:30:10 +0300

    style: use italic styling for empty states and fix JS bug causing duplicate cards

--- backend/static/js/main.js
+++ backend/static/js/main.js
@@ -1,16 +1,16 @@
 // main.js - додаємо імпорт та ініціалізацію
 
-import { initLanguageSwitcher } from "./modules/language-switcher.js";
-import { initScrollIndicator } from "./modules/scroll-indicator.js";
-import { initCustomSelects } from "./modules/custom-select.js";
-import { initCountryFilter } from "./modules/country-filter.js";
-import { initAddUniversity } from "./modules/add-university.js";
-import { initFAQ } from "./modules/faq.js";
-import { initSlider } from "./modules/slider-mobile.js";
-import { modalBtnClose } from "./modules/modal.js";
-import { burgerMenu } from "./modules/burger-menu.js";
-import { initProgramSearch } from "./modules/program-search.js";
-import { initTrimText } from "./modules/trimProgramText.js";
+import { initLanguageSwitcher } from "./modules/language-switcher.js?v=2";
+import { initScrollIndicator } from "./modules/scroll-indicator.js?v=2";
+import { initCustomSelects } from "./modules/custom-select.js?v=2";
+import { initCountryFilter } from "./modules/country-filter.js?v=2";
+import { initAddUniversity } from "./modules/add-university.js?v=2";
+import { initFAQ } from "./modules/faq.js?v=2";
+import { initSlider } from "./modules/slider-mobile.js?v=2";
+import { modalBtnClose } from "./modules/modal.js?v=2";
+import { burgerMenu } from "./modules/burger-menu.js?v=2";
+import { initProgramSearch } from "./modules/program-search.js?v=2";
+import { initTrimText } from "./modules/trimProgramText.js?v=2";
 
 document.addEventListener("DOMContentLoaded", () => {
   initLanguageSwitcher();
